8bdf5536e3c454147dd9b2f2c1f90b95283fbfcf
cannot-fly-pig <document.sabakan@gmail.com>
2020-05-04 01:28:19 +0900

    8日目

--- haribote/bootpack.c
+++ haribote/bootpack.c
@@ -4,13 +4,21 @@
 #include <stdio.h>
 
 extern struct FIFO8 keyfifo, mousefifo;
-void enable_mouse(void);
+
+struct MOUSE_DEC{
+	unsigned char buf[3], phase;
+	int x, y, btn;
+};
+
+void enable_mouse(struct MOUSE_DEC *mdec);
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char data);
 void init_keyboard(void);
 
 void HariMain(void)
 {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
 	char s[40], mcursor[256], keybuf[32], mousebuf[128];
+	unsigned char mouse_dbuf[3], mouse_phase;
 	int mx, my;
 
 	init_gdtidt();
@@ -33,7 +41,8 @@ void HariMain(void)
 	sprintf(s, "(%d, %d)", mx, my);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
-	enable_mouse();
+	struct MOUSE_DEC mdec;
+	enable_mouse(&mdec);
 
 	for (;;) {
 		io_cli();
@@ -49,9 +58,37 @@ void HariMain(void)
 			}else if (fifo8_status(&mousefifo) != 0){
 				int i = fifo8_get(&mousefifo);
 				io_sti();
-				sprintf(s, "%x", i);
-				boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 47, 31);
-				putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+				if (mouse_decode(&mdec, i) != 0){
+					sprintf(s, "[lcr %d %d]", mdec.x, mdec.y);
+					if ((mdec.btn & 0x01) != 0){
+						s[1] = 'L';
+					}
+					if ((mdec.btn & 0x02) != 0){
+						s[3] = 'R';
+					}
+					if ((mdec.btn & 0x04) != 0){
+						s[2] = 'C';
+					}
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 15 * 8 - 1, 31);
+					putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+					/* マウスカーソルの移動 */
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, mx, my, mx + 15, my + 15);
+					mx += mdec.x;
+					my += mdec.y;
+					if (mx < 0){
+						mx = 0;
+					}
+					if (my < 0){
+						my = 0;
+					}
+					if (mx > binfo->scrnx - 16){
+						mx = binfo->scrnx - 16;
+					}
+					if (my > binfo->scrny - 16){
+						my = binfo->scrny - 16;
+					}
+					putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+				}
 			}
 		}
 	}
@@ -84,10 +121,45 @@ void init_keyboard(void) {
 #define KEYCMD_SENDTO_MOUSE		0xd4
 #define MOUSECMD_ENABLE			0xf4
 
-void enable_mouse(void){
+void enable_mouse(struct MOUSE_DEC *mdec){
 	wait_KBC_sebdready();
 	io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
 	wait_KBC_sebdready();
 	io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
+	mdec->phase = 0;
 	return;
+}
+
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char data){
+	if (mdec->phase == 0) {
+		if (data == 0xfa){
+			mdec->phase = 1;
+		}
+		return 0;
+	}else if (mdec->phase == 1) {
+		if ((data & 0xc8) == 0x08) {
+			mdec->buf[0] = data;
+			mdec->phase = 2;
+		}
+		return 0;
+	}else if (mdec->phase == 2) {
+		mdec->buf[1] = data;
+		mdec->phase = 3;
+		return 0;
+	}else if (mdec->phase == 3) {
+		mdec->buf[2] = data;
+		mdec->phase = 1;
+		mdec->btn = mdec->buf[0] & 0x07;
+		mdec->x = mdec->buf[1];
+		mdec->y = mdec->buf[2];
+		if ((mdec->buf[0] & 0x10) != 0) {
+			mdec->x |= 0xffffff00;
+		}
+		if ((mdec->buf[0] & 0x20) != 0) {
+			mdec->y |= 0xffffff00;
+		}
+		mdec->y = - mdec->y;
+		return 1;
+	}
+	return -1;
 }
